dc396be5e7efdeea41904bd9e9faaba0c2bff7e1
Airwoman <airwomanzhy@hotmail.com>
2016-05-31 01:23:08 +1000

    add image load to shops

--- app/controllers/casein/shops_controller.rb
+++ app/controllers/casein/shops_controller.rb
@@ -2,21 +2,21 @@
 
 module Casein
   class ShopsController < Casein::CaseinController
-  
+
     ## optional filters for defining usage according to Casein::AdminUser access_levels
     # before_filter :needs_admin, :except => [:action1, :action2]
     # before_filter :needs_admin_or_current_user, :only => [:action1, :action2]
-  
+
     def index
       @casein_page_title = 'Shops'
   		@shops = Shop.order(sort_order(:name)).paginate :page => params[:page]
     end
-  
+
     def show
       @casein_page_title = 'View shop'
       @shop = Shop.find params[:id]
     end
-  
+
     def new
       @casein_page_title = 'Add a new shop'
     	@shop = Shop.new
@@ -24,7 +24,7 @@ module Casein
 
     def create
       @shop = Shop.new shop_params
-    
+
       if @shop.save
         flash[:notice] = 'Shop created'
         redirect_to casein_shops_path
@@ -33,12 +33,12 @@ module Casein
         render :action => :new
       end
     end
-  
+
     def update
       @casein_page_title = 'Update shop'
-      
+
       @shop = Shop.find params[:id]
-    
+
       if @shop.update_attributes shop_params
         flash[:notice] = 'Shop has been updated'
         redirect_to casein_shops_path
@@ -47,7 +47,7 @@ module Casein
         render :action => :show
       end
     end
- 
+
     def destroy
       @shop = Shop.find params[:id]
 
@@ -55,11 +55,11 @@ module Casein
       flash[:notice] = 'Shop has been deleted'
       redirect_to casein_shops_path
     end
-  
+
     private
-      
+
       def shop_params
-        params.require(:shop).permit(:name, :address, :phone, :website)
+        params.require(:shop).permit(:name, :address, :phone, :website, :image)
       end
 
   end
---
+++ db/migrate/20160530140727_add_iamge_to_shops.rb
@@ -0,0 +1,5 @@
+class AddIamgeToShops < ActiveRecord::Migration
+  def change
+    add_column :shops, :image, :string
+  end
+end
