993811022e0d09bac56618cc11b6d97894c0994d
Airwoman <airwomanzhy@hotmail.com>
2016-05-30 23:18:06 +1000

    add avatar to casein_admin_users

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'web-console', '~> 2.0'
   gem 'pry-rails'
   gem 'annotate'
+  gem 'carrierwave'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530113912) do
+ActiveRecord::Schema.define(version: 20160530115128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160530113912) do
     t.string   "time_zone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   create_table "shops", force: :cascade do |t|
