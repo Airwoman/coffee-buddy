73ea8eebd2473a687051308e81dc0b63ab399644
Airwoman <airwomanzhy@hotmail.com>
2016-07-22 14:49:10 +1000

    carrierwave setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
   include Cloudinary::CarrierWave
 
   # Include RMagick or MiniMagick support:
@@ -8,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -48,5 +49,26 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+
+  # Crop to 275px, 206px
+version :thumbnail do
+    process resize_to_fill: [275, 206]
+end
+
+# crop to 62px, 62px
+version :mini_tumbnail do
+    process resize_to_fill: [62, 62]
+end
+
+# crop to 870px, 261px
+version :detail do
+    process resize_to_fill: [870, 261]
+end
+
+# Allowed image format
+def extension_white_list
+     %w(jpg jpeg gif png)
+end
 
 end
